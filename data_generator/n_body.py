62979dcaf5e2db332ace2fe1bb150b5113524b20
Reuben Brasher <rb@terraquantum.swiss>
2024-04-29 16:33:14 -0700

    Generate dataframe of observed forces

--- data_generator/n_body.py
+++ data_generator/n_body.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from typing import Dict, Type, List, Tuple
 from dataclasses import dataclass, field
 
@@ -14,10 +15,36 @@ def compute_force(m0: float, m1: float, r: np.ndarray) -> np.ndarray:
     return -(m0 * m1 / np.linalg.norm(r) ** 3) * r
 
 
+def random_conformations(m: int, n: int, rng: np.random.Generator, scale) -> np.ndarray:
+    """
+    Produce m conformations of n particles
+    :param m: number of conformations
+    :param n: number of particles
+    :param rng: random number generator
+    :param scale: scale of conformations
+    :return: m x n x 3 dimensional array
+    """
+    return scale * rng.uniform(-1, 1, size=[m, n, 3])
+
+
+def coords2dataframe(r: np.ndarray) -> pd.DataFrame:
+    d = {}
+    for i in range(r.shape[1]):
+        for j in range(3):
+            letter = "xyz"[j]
+            key = f"{letter}{i+1}"
+            d[key] = r[:, i, j]
+    return pd.DataFrame(d)
+
+
 @dataclass
 class NBody:
     m0: float
     m: List[float]
+
+    @property
+    def n(self):
+        return len(self.m)
 
     def total_force(self, r: np.ndarray) -> np.ndarray:
         """
@@ -26,7 +53,25 @@ class NBody:
         :return: total force on particle 0
         """
         f = np.zeros(3)
-        for i in range(len(self.m)):
+        for i in range(self.n):
             f += compute_force(self.m0, self.m[i], r[i])
 
         return f
+
+    def generate_random_observations(
+        self, m: int, rng: np.random.Generator, scale: float, epsilon: float
+    ) -> pd.DataFrame:
+        """
+        Generate random conformations, record perturbed positions and computed magnitude of fource
+        :param m: number of observations
+        :param scale: scale of random positions
+        :param epsilon: scale of observation error of positions
+        :return: table of observations where last column is magnitude of force
+        """
+        r = random_conformations(m, self.n, rng, scale)
+        delta = random_conformations(m, self.n, rng, epsilon)
+        df = coords2dataframe(r + delta)
+        forces = [self.total_force(row) for row in r]
+        forces = [np.linalg.norm(f) for f in forces]
+        df["forces"] = forces
+        return df
--- data_generator/tests/test_n_body.py
+++ data_generator/tests/test_n_body.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import pandas as pd
 
 from .. import n_body
 
@@ -32,6 +33,13 @@ def test_compute_force():
     np.testing.assert_array_almost_equal(expected, actual)
 
 
+def test_nbody():
+    nb = n_body.NBody(2, [3, 5, 7, 11])
+    assert nb.m0 == 2
+    assert nb.m == [3, 5, 7, 11]
+    assert nb.n == 4
+
+
 def test_total_force():
     nb = n_body.NBody(2, [3, 5, 7])
     r = np.array([[1, 0, 0], [0, -1, 0], [0, 0, 1]])
@@ -48,3 +56,108 @@ def test_total_force():
     actual = nb.total_force(r)
     expected = np.array([1.1052651820707768, -1.1596390090089517, -1.9946552655847771])
     np.testing.assert_array_almost_equal(expected, actual)
+
+    nb.m.append(11)
+    r = np.array([[1.5, 1, 0.3], [3, -1, -1.5], [-1.6, 0.2, 1], [1, 0, 0]])
+    actual = nb.total_force(r)
+    expected += np.array([-22, 0, 0])
+    np.testing.assert_array_almost_equal(expected, actual)
+
+
+def test_random_conformation():
+    actual = n_body.random_conformations(4, 2, np.random.default_rng(42), 3)
+    expected = np.array(
+        [
+            [
+                [1.64373629133578, -0.3667293614876861, 2.1515875194682947],
+                [1.1842081743561834, -2.434935912674103, 2.8537341098205355],
+            ],
+            [
+                [1.5668382119421178, 1.7163858316617229, -2.2313182039467248],
+                [-0.2976843726265972, -0.7752118546045126, 2.560589933091611],
+            ],
+            [
+                [0.8631907204839873, 1.9365696796249798, -0.3395148070360132],
+                [-1.6365676692913387, 0.3275087220950088, -2.6170964633749483],
+            ],
+            [
+                [1.9657870319554924, 0.7899863947323891, 1.548526440512243],
+                [-0.8728441912207898, 2.8241881463694196, 2.358726727933186],
+            ],
+        ]
+    )
+    np.testing.assert_array_almost_equal(expected, actual)
+
+
+def test_coords2dataframe():
+    r = [
+        [[1, 2, 3], [4, 5, 6]],
+        [[7, 8, 9], [10, 11, 12]],
+        [[13, 14, 15], [16, 17, 18]],
+        [[19, 20, 21], [22, 23, 24]],
+    ]
+    r = np.array(r)
+    expected = {
+        "x1": [1, 7, 13, 19],
+        "y1": [2, 8, 14, 20],
+        "z1": [3, 9, 15, 21],
+        "x2": [4, 10, 16, 22],
+        "y2": [5, 11, 17, 23],
+        "z2": [6, 12, 18, 24],
+    }
+    expected = pd.DataFrame(expected)
+    actual = n_body.coords2dataframe(r)
+    pd.testing.assert_frame_equal(expected, actual)
+
+
+def test_generate_random_observations():
+    nb = n_body.NBody(2, [3, 5])
+    actual = nb.generate_random_observations(
+        4, np.random.default_rng(42), scale=2.5, epsilon=0.1
+    )
+    expected = {
+        "x1": {
+            0: 1.425456942194569,
+            1: 1.3546509411333283,
+            2: 0.645309901470417,
+            3: 1.6782088803633604,
+        },
+        "y1": {
+            0: -0.3666800596693449,
+            1: 1.5238234728716111,
+            2: 1.6089490515993365,
+            3: 0.6207953238867324,
+        },
+        "z1": {
+            0: 1.786333800302319,
+            1: -1.8942667649946403,
+            2: -0.33754713605316755,
+            3: 1.3568906607059095,
+        },
+        "x2": {
+            0: 0.8956008984542653,
+            1: -0.27397836931519054,
+            2: -1.3298435921396135,
+            3: -0.6664172878512978,
+        },
+        "y2": {
+            0: -2.098255362148243,
+            1: -0.6520987165819322,
+            2: 0.26035431885364013,
+            3: 2.3309857977805515,
+        },
+        "z2": {
+            0: 2.4147215488322704,
+            1: 2.071719216059866,
+            2: -2.1143780802675556,
+            3: 1.9232712273970372,
+        },
+        "forces": {
+            0: 2.00113252405678,
+            1: 1.3330457063078516,
+            2: 2.416168168305407,
+            3: 1.9102618773856046,
+        },
+    }
+    expected = pd.DataFrame(expected)
+    pd.testing.assert_frame_equal(expected, actual)
